f93f725be964e48cae610426f04f791ec15c76c7
Fabiano <Fabiano.bonfa@gmail.com>
2026-03-09 22:42:27 -0300

    URL Nuvem

--- src/services/constantes.jsx
+++ src/services/constantes.jsx
@@ -1,5 +1,6 @@
 // src/services/constantes.jsx
-export const BASE_URL = "http://localhost:8051";
+//export const BASE_URL = "http://localhost:8051";
+export const BASE_URL = 'http://44.223.22.69:8051/';
 
 // Armazena o token de acesso
 export const SESSAO = {
