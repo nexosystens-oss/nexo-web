573b6adcf6f00b2a0fe79f44567693ec41eb6a1b
Fabiano <Fabiano.bonfa@gmail.com>
2026-06-27 14:34:45 -0400

    .

--- src/services/constantes.jsx
+++ src/services/constantes.jsx
@@ -1,6 +1,6 @@
 // src/services/constantes.jsx
 //export const BASE_URL = "http://localhost:8051";
-export const BASE_URL = 'http://nexoapp.com.br:8051/';
+export const BASE_URL = 'https://nexoapp.com.br:8051/';
 
 // Armazena o token de acesso
 export const SESSAO = {
